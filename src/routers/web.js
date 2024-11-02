68fd0a300900e9cc671cf8c0a15500c733728706
khanhquang234 <root@DESKTOP-TIANHJ0>
2024-11-03 04:08:29 +0700

    Add webhook handlers

--- src/routers/web.js
+++ src/routers/web.js
@@ -1,14 +1,8 @@
 import express from "express";
-import request from 'request';
 
 let router = express.Router();
 
 let initWebRoutes = (app) => {
-    // Route mặc định
-    router.get("/", (req, res) => {
-        return res.send("Hello World");
-    });
-
     // Webhook verification
     router.get("/webhook", (req, res) => {
         let mode = req.query["hub.mode"];
@@ -25,19 +19,20 @@ let initWebRoutes = (app) => {
         }
     });
 
-    // Xử lý các sự kiện webhook
+    // Handle messages
     router.post("/webhook", (req, res) => {
         let body = req.body;
 
         if (body.object === "page") {
             body.entry.forEach(function(entry) {
                 let webhook_event = entry.messaging[0];
+                console.log(webhook_event);
+
                 let sender_psid = webhook_event.sender.id;
+                console.log('Sender PSID: ' + sender_psid);
 
                 if (webhook_event.message) {
                     handleMessage(sender_psid, webhook_event.message);
-                } else if (webhook_event.postback) {
-                    handlePostback(sender_psid, webhook_event.postback);
                 }
             });
 
@@ -50,46 +45,13 @@ let initWebRoutes = (app) => {
     return app.use("/", router);
 };
 
-// Xử lý tin nhắn
 function handleMessage(sender_psid, received_message) {
-    let response;
-    if (received_message.text) {    
-        response = {
-            "text": `Bạn vừa gửi: "${received_message.text}"`
-        }
-    }  
-    callSendAPI(sender_psid, response);    
-}
-
-// Xử lý postback
-function handlePostback(sender_psid, received_postback) {
     let response = {
-        "text": "Cảm ơn!"
-    }
-    callSendAPI(sender_psid, response);
-}
-
-// Gửi phản hồi qua Facebook API
-function callSendAPI(sender_psid, response) {
-    let request_body = {
-        "recipient": {
-            "id": sender_psid
-        },
-        "message": response
-    }
-
-    request({
-        "uri": "https://graph.facebook.com/v2.6/me/messages",
-        "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
-        "method": "POST",
-        "json": request_body
-    }, (err, res, body) => {
-        if (!err) {
-            console.log('Message sent!')
-        } else {
-            console.error("Unable to send message:" + err);
-        }
-    }); 
+        "text": `Bạn vừa gửi: "${received_message.text}"`
+    };
+    
+    // Gửi response qua Facebook API
+    // Bạn sẽ cần thêm hàm callSendAPI ở đây
 }
 
 export default initWebRoutes;
